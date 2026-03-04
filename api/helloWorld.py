c4503ce92df2accfb3d1b38f6f95410a2aa4da48
Bruno Zegada <javierzegada@Brunos-MacBook-Pro.local>
2026-03-03 23:04:55 -0500

    I started a basic server that says hello world for examples

---
+++ api/helloWorld.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
+import os
+from fastapi.responses import JSONResponse
+from fastapi import status
+
+router = APIRouter()
+
+
+
+
+
+@router.get("/api/helloWorld")
+async def getChapters_endpoint():
+    '''
+    Small example of hello world
+    '''
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"response": "nivar"}
+    )
